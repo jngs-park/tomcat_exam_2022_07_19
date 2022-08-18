052bd7c6e6849904e34dae5cc5fd551ebae510cb
jngs-park <dear.jayp@gmail.com>
2022-08-19 02:18:32 +0900

    rq 도입

---
+++ src/main/java/com/ll/exam/GugudanServlet.java
@@ -0,0 +1,35 @@
+package com.ll.exam;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+
+@WebServlet("/gugudan")
+public class GugudanServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Rq rq = new Rq(req, resp);
+
+        req.setCharacterEncoding("UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/h tml; charset=utf-8");
+
+        int dan = rq.getIntParam("dan", 0);
+        int limit = rq.getIntParam("limit", 0);
+
+        rq.appendBody("<h1>%d단</h1>\n".formatted(dan));
+
+        for (int i = 1; i <= limit; i++) {
+            rq.appendBody("<div>%d * %d = %d</div>\n".formatted(dan, i, dan * i));
+        }
+    }
+}
+
+
+ // http://localhost:8081/gugudan?dan=44&limit=1111
